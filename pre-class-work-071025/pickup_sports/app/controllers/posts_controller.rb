9b25d6c2c70b7183352a69b51131680b71100058
sjtroxel <sjtroxel@alumni.iu.edu>
2025-07-06 13:46:51 -0500

    first commit starter files to work thru videos

---
+++ pre-class-work-071025/pickup_sports/app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:update, :destroy]
+
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post, status: :created
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post, status: :ok
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      render json: nil, status: 200
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.permit(:content, :user_id)
+  end
+end
---
+++ pre-class-work-071025/pickup_sports/app/models/event.rb
@@ -0,0 +1,14 @@
+class Event < ApplicationRecord
+  # validations
+  validates :start_date_time, :end_date_time, :guests, presence: true
+
+  # associations
+  belongs_to :user
+  has_one :location, as: :locationable, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+
+  has_many :event_participants
+  has_many :users, through: :event_participants
+
+  has_and_belongs_to_many :sports
+end
