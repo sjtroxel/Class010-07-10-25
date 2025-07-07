ee468f36c791eb10c8b43311f897d7c2de787b08
sjtroxel <sjtroxel@alumni.iu.edu>
2025-07-06 23:03:36 -0500

    finished the events controller test video with a little help from CodeLabs GPT

--- pre-class-work-071025/pickup_sports/config/routes.rb
+++ pre-class-work-071025/pickup_sports/config/routes.rb
@@ -1,22 +1,7 @@
 Rails.application.routes.draw do
-  # # localhost:3000/users
-  # get '/users', to: 'users#index'
-
-  # # localhost:3000/users/1
-  # get '/users/:id', to: 'users#show'
-
-  # # localhost:3000/users
-  # post '/users', to: 'users#create'
-
-  # # localhost:3000/users/1
-  # put '/users/:id', to: 'users#update'
-
-  # # localhost:3000/users/1
-  # delete '/users/:id', to: 'users#destroy'
-
+  resources :events
+  resources :posts
   resources :users do
     get 'posts', to: "users#posts_index"
   end
-
-  resources :posts
 end
--- pre-class-work-071025/pickup_sports/spec/models/event_spec.rb
+++ pre-class-work-071025/pickup_sports/spec/models/event_spec.rb
@@ -45,4 +45,14 @@ RSpec.describe Event, type: :model do
       expect(event.sports.count).to eq(3)
     end
   end
+
+  context "destroy related associations" do
+    it "destroy event participants" do
+      event = create(:event)
+      event_id = event.id
+      event.destroy
+      event_participants = EventParticipant.where(event_id: event.id)
+      expect(event_participants).to be_empty
+    end
+  end
 end
